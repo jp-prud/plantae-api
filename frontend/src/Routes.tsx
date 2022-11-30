87e693bc5a1871222fe24825ff5b953b53fc6672
João Pedro Prudêncio <jprudencio@maeztra.com>
2022-11-29 23:47:19 -0300

    feat: define new routes

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -8,16 +8,20 @@ import QualitySeal from './pages/QualitySeal';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
+import PrivateRoutes from './utils/PrivateRoute';
+
 export default function Routes() {
   return (
     <Switch>
       <Route path="/">
-        <Route index element={<Home />} />
-        <Route path="info" element={<Infos />} />
+        <Route element={<PrivateRoutes />}>
+          <Route index element={<Home />} />
+          <Route path="infos" element={<Infos />} />
 
-        <Route path="product" element={<Product />} />
-        <Route path="productive-locale" element={<ProductiveLocale />} />
-        <Route path="quality-seal" element={<QualitySeal />} />
+          <Route path="product" element={<Product />} />
+          <Route path="productive-locale" element={<ProductiveLocale />} />
+          <Route path="quality-seal" element={<QualitySeal />} />
+        </Route>
 
         <Route path="sign-in" element={<SignIn />} />
         <Route path="sign-up" element={<SignUp />} />
